02c6df096653180d602327b736261c0ea6f46293
ScrapNoodles <144817703+ScrapNoodles@users.noreply.github.com>
2026-08-04 04:04:26 -0400

    edited part 3 code

--- lab_4/part3/script.js
+++ lab_4/part3/script.js
@@ -12,15 +12,18 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
 
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
+
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// function to generate random color
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
 
 function randomRGB() {
   return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
