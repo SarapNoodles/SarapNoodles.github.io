ecdd287e8d43584cfbb18e0d7360e62fa4c23b1b
ScrapNoodles <144817703+ScrapNoodles@users.noreply.github.com>
2026-08-04 03:06:01 -0400

    edited html for part 2

--- lab_4/part2/main.js
+++ lab_4/part2/main.js
@@ -1,9 +1,8 @@
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
-
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
-const baseURL = 'images/'; 
+
 
 const images= [
     { filename: 'pic1.jpg', alt: 'Close-up of a human eye'},
@@ -12,6 +11,28 @@ const images= [
     { filename: 'pic4.jpg', alt: 'Section of wall from a pharohs tomb'},
     { filename: 'pic5.jpg', alt: 'Large moth on a leaf'},
     ];
+
+const baseURL = 'images/'; 
+
+const displayedImage = document.quereySelector('.displayed-img')
+function updateDisplayedImage(filename, altText){
+    displayedImage.src = 'images/${filename}';
+    displayedImage.alt = altText;
+}
+
+btn.addEventListener('click',() => {
+    if(btn.classList.contains('light')){
+        btn.classList.remove('light')
+        btn.classList.add('dark')
+        btn.textContent = 'Lighten';
+        overlay.computedStyleMap.backgroundColor = 'rgba(0,0,0,0.5)';
+    }else{
+        btn.classList.remove('dark')
+        btn.classList.add('light')
+        btn.textContent = 'Darken';
+        overlay.computedStyleMap.backgroundColor = 'rgba(0,0,0,0)';
+    }
+})
 
 for (const image of images){
     const newImage = document.createElement('img');
@@ -25,6 +46,11 @@ for (const image of images){
     //updates displayed image every click
     newImage.addEventListener('click',() => {updateDisplayedImage(image.filename,image.alt);});
 
+    newImage.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter'){
+            updateDisplayedImage(image.filename, image.alt)
+        }
+    });
 
 }
 
